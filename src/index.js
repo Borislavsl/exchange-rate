19aeefa2b285b279110787a2fab5bb73826d09ae
Borislav Slavov <bslavov@nemetschek.bg>
2021-06-15 16:15:06 +0300

    redux hooks

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 import { ExchangeRate } from "./components/ExchangeRate";
 import "./style.css";
+import { getInitialRates } from "./store/rates";
 
-ReactDOM.render(<ExchangeRate />, document.getElementById("root"));
+// kick start AJAX call for exchange rates
+store.dispatch(getInitialRates);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ExchangeRate />
+    </Provider>,
+    document.getElementById("root"));
---
+++ src/store/rates.js
@@ -0,0 +1,91 @@
+import { getExchangeRates } from "../api";
+
+const initialState = {
+    amount: "12.00",
+    currencyCode: "USD",
+    currencyData: { USD: { displayLabel: "US Dollars", code: "USD", rate: 1.0 } },
+    supportedCurrencies: ["USD", "EUR", "JPY", "CAD", "GBP", "MXN"]
+}
+
+// action types
+const AMOUNT_CHANGED = "rates/amountChanged";
+const CURRENCY_CODE_CHANGED = "rates/currencyCodeChanged";
+const RATES_RECEIVED = "rates/ratesReceived";
+const LABEL_RECEIVED = "rates/labelReceived";
+
+export function ratesReducer(state = initialState, action) {
+    switch (action.type) {
+        case AMOUNT_CHANGED:
+            return { ...state, amount: action.payload };
+        case CURRENCY_CODE_CHANGED:
+            return { ...state, currencyCode: action.payload };
+        case LABEL_RECEIVED: {
+            const { displayLabel, currencyCode } = action.payload;
+            return {
+                ...state,
+                currencyData: {
+                    ...state.currencyData,
+                    [currencyCode]: {
+                        ...state.currencyData[currencyCode],
+                        displayLabel
+                    }
+                }
+            }
+        }
+        case RATES_RECEIVED: {
+                const codes = Object.keys(action.payload).concat(state.currencyCode);
+                const currencyData = {};
+                for (let code in action.payload) {
+                    currencyData[code] =  { code, rate: action.payload[code] };
+                }
+                return {
+                    ...state,
+                    currencyData,
+                    supportedCurrencies: codes
+                };
+            }
+        default:
+            return state;
+    }
+}
+
+// selectors
+export const getAmount = (state) => state.rates.amount;
+export const getCurrencyCode = (state) => state.rates.currencyCode;
+export const getCurrencyData = (state) => state.rates.currencyData;
+export const getSupportedCurrencies = (state) => state.rates.supportedCurrencies;
+export const getDisplayLabel = (state, currencyCode) => {
+    const match = state.rates.currencyData[currencyCode];
+    if (match) return match.displayLabel;
+}
+
+// action creators
+export const changeAmount = (amount) => ({
+    type: AMOUNT_CHANGED,
+    payload: amount,
+});
+
+export function changeCurrencyCode(currencyCode) {
+    return function changeCurrencyCodeThunk(dispatch, getState) {
+        const state = getState();
+        const supportedCurrencies = getSupportedCurrencies(state);
+        dispatch({
+            type: CURRENCY_CODE_CHANGED,
+            payload: currencyCode
+        });
+        getExchangeRates(currencyCode, supportedCurrencies)
+            .then((rates) => {
+                dispatch({
+                    type: RATES_RECEIVED,
+                    payload: rates
+                })
+            });
+    };
+}
+
+// thunks
+export function getInitialRates(dispatch, getState) {
+    const state = getState();
+    const currencyCode = getCurrencyCode(state);
+    dispatch(changeCurrencyCode(currencyCode));
+}
